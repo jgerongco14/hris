804df361d8987f4a3d216fe0782666f6d0331e1b
Rendel Jeff Gerongco <jgerongco14@gmail.com>
2025-09-28 12:56:46 +0800

    Move Laravel app from hris-app to project root

--- hris-app/resources/views/components/loading.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- Loading Spinner -->
-<div id="loading-spinner" style="display:none;">
-    <div class="spinner">Loading...</div> <!-- Customize this spinner -->
-</div>
-
-
--- hris-app/resources/views/components/titlebar.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="titlebar row align-items-center border">
-    <div class="col text-start my-2">
-        <h1 class="font-weight-bold">HUMAN RESOURCE INFORMATION SYSTEM</h1>
-    </div>
-    <div class="col-2 text-end my-2">
-        <a href="{{ route('logout') }}" class="btn btn-danger">Logout</a>
-    </div>
-</div>
